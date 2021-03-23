341e5147991e95d560b521b92c80bcda528b5a81
snasam <77593263+snasam@users.noreply.github.com>
2021-03-23 01:29:16 -0500

    users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var x=0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  x++;
+  res.send('User accesses are:'+ x);
 });
 
 module.exports = router;
